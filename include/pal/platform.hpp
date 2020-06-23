40ce6826a40c16dcba94c3d00a8a18554a1b142d
Bastian Blokland <bastianblokland@gmail.com>
2020-06-23 20:50:55 +0300

    Fail compilation on supported platform

--- include/pal/platform.hpp
+++ include/pal/platform.hpp
@@ -3,4 +3,6 @@
 
 #if PLATFORM_LINUX
 #include "platform.linux.xcb.hpp"
+#else
+static_assert(false, "Unsupported platform");
 #endif
--- include/pal/window.hpp
+++ include/pal/window.hpp
@@ -3,4 +3,6 @@
 
 #if PLATFORM_LINUX
 #include "window.linux.xcb.hpp"
+#else
+static_assert(false, "Unsupported platform");
 #endif
