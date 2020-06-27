990e8d626d98a50f326df679f82116baa35e56f6
Bastian Blokland <bastianblokland@gmail.com>
2020-06-27 20:37:22 +0300

    Add pal api to get path to exe

--- include/pal/utils.hpp
+++ include/pal/utils.hpp
@@ -1,8 +1,11 @@
 #pragma once
+#include "fs.hpp"
 #include <string>
 #include <string_view>
 
 namespace pal {
+
+auto getCurExecutablePath() noexcept -> fs::path;
 
 auto setThreadName(std::string_view name) noexcept -> bool;
 
