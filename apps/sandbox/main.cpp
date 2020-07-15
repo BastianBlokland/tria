857b9f5a47d61e3bd96a5cf594d5fc2b776d3745
Bastian Blokland <bastianblokland@gmail.com>
2020-07-15 23:40:01 +0300

    Cleanup of sandbox main.cpp

--- apps/sandbox/main.cpp
+++ apps/sandbox/main.cpp
@@ -1,63 +1,32 @@
 #include "tria/asset/database.hpp"
 #include "tria/gfx/context.hpp"
-#include "tria/log/api.hpp"
 #include "tria/pal/interrupt.hpp"
 #include "tria/pal/platform.hpp"
 #include "tria/pal/utils.hpp"
-#include <chrono>
-#include <sstream>
-#include <stdexcept>
 #include <thread>
 
 using namespace std::literals;
 using namespace tria;
 
-using Clock    = std::chrono::high_resolution_clock;
-using Duration = std::chrono::duration<double>;
+auto runApp(pal::Platform& platform, asset::Database& db, gfx::Context& gfx) {
 
-auto operator<<(std::ostream& out, const Duration& rhs) -> std::ostream& {
-  auto s = rhs.count();
-  if (s < .001) {
-    out << s * 1000000 << " us";
-  } else {
-    out << s * 1000 << " ms";
-  }
-  return out;
-}
+  auto win    = platform.createWindow(512, 512);
+  auto canvas = gfx.createCanvas(&win, gfx::VSyncMode::Disable);
 
-auto runApp(log::Logger& logger, pal::Platform& platform) -> int {
+  const auto* triangle = db.get("triangle.gfx")->downcast<asset::Graphic>();
+  const auto* quad     = db.get("quad.gfx")->downcast<asset::Graphic>();
 
-  auto assetDb    = asset::Database{&logger, pal::getCurExecutablePath().parent_path() / "data"};
-  auto gfxContext = gfx::Context{&logger};
-  auto mainWin    = platform.createWindow(512, 512);
-  auto mainCanvas = gfxContext.createCanvas(&mainWin, gfx::VSyncMode::Disable);
-
-  const auto* triangle = assetDb.get("triangle.gfx")->downcast<asset::Graphic>();
-  const auto* quad     = assetDb.get("quad.gfx")->downcast<asset::Graphic>();
-
-  auto frameBegin = Clock::now();
-  while (!mainWin.getIsCloseRequested() && !pal::isInterruptRequested()) {
-
-    // Process platform events.
+  while (!win.getIsCloseRequested() && !pal::isInterruptRequested()) {
     platform.handleEvents();
 
-    if (mainCanvas.drawBegin()) {
-      mainCanvas.draw(triangle, 3);
-      mainCanvas.draw(quad, 6);
-      mainCanvas.drawEnd();
+    if (canvas.drawBegin()) {
+      canvas.draw(triangle, 3);
+      canvas.draw(quad, 6);
+      canvas.drawEnd();
     } else {
       // Unable to draw, possibly due to a minimized window.
       std::this_thread::sleep_for(100ms);
     }
-
-    auto frameEnd = Clock::now();
-    auto dur      = frameEnd - frameBegin;
-    frameBegin    = frameEnd;
-
-    // Update window title.
-    std::stringstream titleStream;
-    titleStream << "Sandbox - " << mainWin.getWidth() << "x" << mainWin.getHeight() << " " << dur;
-    mainWin.setTitle(titleStream.str());
   }
 
   return 0;
@@ -68,18 +37,24 @@ auto main(int /*unused*/, char* * /*unused*/) -> int {
   pal::setThreadName("tria_main_thread");
   pal::setupInterruptHandler();
 
-  auto logger = log::Logger{log::makeConsolePrettySink(), log::makeFileJsonSink("sandbox.log")};
+  auto logger   = log::Logger{log::makeConsolePrettySink(), log::makeFileJsonSink("sandbox.log")};
+  auto platform = pal::Platform{&logger};
+  auto db       = asset::Database{&logger, pal::getCurExecutablePath().parent_path() / "data"};
+  auto gfx      = gfx::Context{&logger};
+
   LOG_I(&logger, "Sandbox startup");
 
-  auto platform = pal::Platform{&logger};
+  int ret;
   try {
-    runApp(logger, platform);
+    ret = runApp(platform, db, gfx);
   } catch (const std::exception& e) {
     LOG_E(&logger, "Uncaught exception", {"what", e.what()});
+    ret = 1;
   } catch (...) {
     LOG_E(&logger, "Uncaught exception");
+    ret = 1;
   }
 
   LOG_I(&logger, "Sandbox shutdown");
-  return 0;
+  return ret;
 }
