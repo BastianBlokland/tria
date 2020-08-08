fb52e4ee247f971b4463949ff8ca3010c4cb977c
Bastian Blokland <bastianblokland@gmail.com>
2020-08-08 19:24:22 +0300

    Mark instance data as readonly
    Possible already implicitly readonly in glsl? But in any case
    this doesn't hurt.

--- apps/sandbox_data/shaders/include/instance.glsl
+++ apps/sandbox_data/shaders/include/instance.glsl
@@ -5,7 +5,7 @@
 const int maxInstances = 2048;
 
 #define INSTANCE_INPUT_BINDING(SET, DATA)                                                          \
-  layout(std140, binding = 0, SET) uniform UniformData { DATA[maxInstances] insts; }               \
+  layout(std140, binding = 0, SET) readonly uniform UniformData { DATA[maxInstances] insts; }      \
   uniformInstances
 
 #define GET_INST() uniformInstances.insts[gl_InstanceIndex]
